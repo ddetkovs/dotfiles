91d0bf7d3caf35df8dc20c2ed9744fdfec626571
Deniss Detkovs <ddetkovs@gmail.com>
2014-08-29 21:16:29 +0100

    home

--- .my_environment.sh
+++ .my_environment.sh
@@ -1,7 +1,17 @@
-# User configuration
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+export COCOS_CONSOLE_ROOT=/home/dinja/workspace/cocos2d-x/tools/cocos2d-console/bin
+export PATH=$COCOS_CONSOLE_ROOT:$PATH
 
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-export PHANTOMJS_BIN="/usr/bin/phantomjs"
+# Add environment variable ANT_ROOT for cocos2d-x
+export ANT_ROOT=/usr/share/ant/bin
+export PATH=$ANT_ROOT:$PATH
 
-alias mount_win="sudo mount -t vboxsf E_DRIVE /media/sf_e_drive"
+export COCOS2DX_ROOT=~/workspace/libs/cocos2d-x-3.0beta2
+export ANDROID_SDK_ROOT=~/workspace/libs/adt-bundle-linux-x86_64-20131030/sdk
+export ANDROID_HOME=ANDROID_SDK_ROOT
+export NDK_ROOT=~/workspace/libs/android-ndk-r9c
+export NDK_TOOLCHAIN_VERSION=4.8
+
+export PATH=${PATH}:${ANDROID_SDK_ROOT}/tools
+export PATH=${PATH}:${ANDROID_SDK_ROOT}/platform-tools
 
